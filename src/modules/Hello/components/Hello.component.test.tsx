a7a5127031e7c0e1768895121a09068628a33296
bladimir.delbarrio <bladimaltego@gmail.com>
2020-06-29 11:13:00 +0200

    feat: added test renders the correct text with an explicit enthusiasm of 1 in Hello.component

--- src/modules/Hello/components/Hello.component.test.tsx
+++ src/modules/Hello/components/Hello.component.test.tsx
@@ -3,12 +3,13 @@ import * as Enzyme from "enzyme";
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-import Provider from 'react-redux'
+import {Provider} from 'react-redux'
 import store from '../../../store/store'
 
 import Hello from "./Hello.component";
 import * as Helloactions from '../actions/Hello.actions'
 import * as Hellotypes from '../types/Hello.types'
+import { render } from "@testing-library/react";
 
 configure({ adapter: new Adapter() });
 
@@ -17,13 +18,21 @@ configure({ adapter: new Adapter() });
 
 
 
+describe('renders Hello component ', () => {
+    it("renders the correct text with an explicit enthusiasm of 1", () => {
+        
+        const wrapper = Enzyme.shallow(
+           <Provider store={store}>
+               <Hello name="Bladi" enthusiasmLevel={1} />
+           </Provider> 
+            
+        ).dive()
+        expect(wrapper).toMatchSnapshot()
+      });
+})
 
 
-/* it("renders the correct text with an explicit enthusiasm of 1", () => {
-  const hello = Enzyme.shallow(<Hello name="Bladi" enthusiasmLevel={1} />);
-  expect(hello.find(".greeting").text()).toEqual("Hello Bladi!");
-});
-
+/*
 it("renders the correct text with an explicit enthusiasm level of 5", () => {
   const hello = Enzyme.shallow(<Hello name="Bladi" enthusiasmLevel={5} />);
   expect(hello.find(".greeting").text()).toEqual("Hello Bladi!!!!!");
@@ -44,8 +53,6 @@ it("throws when the enthusiasm level is negative", () => {
 
 describe('Hello.actions', () => {
     it('should create an action SEND_MESSAGE', () => {
-      const message = 'Bladi'
-      const enthusiasmLevel = 1
       const data:Hellotypes.Message = {
           message:'Bladi',
           enthusiasmLevel:1
